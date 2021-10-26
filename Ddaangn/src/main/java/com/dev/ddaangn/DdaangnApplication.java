22034de38933d78135578364591942d2b9f108b7
sds1vrk <sdsvrk@gmail.com>
2021-10-26 14:42:28 +0900

    init project

---
+++ Ddaangn/src/main/java/com/dev/ddaangn/DdaangnApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ddaangn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaangnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdaangnApplication.class, args);
+    }
+
+}
---
+++ Ddaangn/src/test/java/com/dev/ddaangn/DdaangnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ddaangn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaangnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
