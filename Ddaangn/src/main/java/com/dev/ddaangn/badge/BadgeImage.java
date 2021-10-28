b1e757d637815c120b716332d4f063f20161f6ce
wooko5 <wooko5@naver.com>
2021-10-28 17:30:34 +0900

    REFACTOR: 주석처리한 코드 삭제

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/BadgeImage.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/BadgeImage.java
@@ -3,20 +3,12 @@ package com.dev.ddaangn.badge;
 import com.dev.ddaangn.image.Image;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
 @Entity(name = "badge_images")
 public class BadgeImage extends Image {
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//
-//    @Column(name = "url", length = 500)
-//    private String url;
 
     // BadgeImage - Badge
     @ManyToOne(fetch = FetchType.LAZY)
