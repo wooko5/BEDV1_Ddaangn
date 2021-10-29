f1c2b92fe3f145fc474eb0262731edeb7e19f8b3
dlfdyd96 <1yongs_@naver.com>
2021-10-29 16:12:40 +0900

    DDAAN-100 service + test

--- Ddaangn/src/main/java/com/dev/ddaangn/post/service/PostService.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/post/service/PostService.java
@@ -5,10 +5,13 @@ import com.dev.ddaangn.common.error.exception.NotFoundException;
 import com.dev.ddaangn.post.converter.PostConverter;
 import com.dev.ddaangn.post.domain.Post;
 import com.dev.ddaangn.post.dto.request.PostInsertRequest;
+import com.dev.ddaangn.post.dto.response.PostDetailResponse;
 import com.dev.ddaangn.post.dto.response.PostInsertResponse;
 import com.dev.ddaangn.post.repository.PostRepository;
 import com.dev.ddaangn.user.User;
 import com.dev.ddaangn.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,6 +42,12 @@ public class PostService {
     public User getUser(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_EXIST_MEMBER));
+    }
+
+    @Transactional
+    public Page<PostDetailResponse> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable)
+                .map(PostDetailResponse::new);
     }
 
 }
