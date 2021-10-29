0c717166df92cb399daf11b7fdb2477b3f36fcf1
wooko5 <wooko5@naver.com>
2021-10-29 16:15:18 +0900

    FEAT: 단일, 전체 조회를 테스트 코드 작성

--- Ddaangn/src/test/java/com/dev/ddaangn/image/service/ImageServiceTest.java
+++ Ddaangn/src/test/java/com/dev/ddaangn/image/service/ImageServiceTest.java
@@ -2,10 +2,19 @@ package com.dev.ddaangn.image.service;
 
 import com.dev.ddaangn.image.repository.ImageRepository;
 import com.dev.ddaangn.image.dto.ImageDto;
+import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -20,12 +29,12 @@ class ImageServiceTest {
 
     private Long id = 1L;
 
-    @Test
-    void save(){
+    @BeforeEach
+    void setUp(){
         // Given
         ImageDto imageDto = ImageDto.builder()
                 .id(id)
-                .url("https://wooko5.com")
+                .url("C:/Users/wooko/desktop/image-20211004165424872.png")
                 .type("BADGE")
                 .build();
 
@@ -36,6 +45,48 @@ class ImageServiceTest {
         assertThat(savedId).isEqualTo(id);
     }
 
+    @AfterEach
+    void cleanUp(){
+        imageRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("단일 조회를 테스트합니다.")
+    void findByIdTest() throws NotFoundException {
+        // Given
+        Long imageId = id;
+
+        // When
+        ImageDto imageDto = imageService.findOneById(imageId);
+
+        // Then
+        assertThat(imageDto.getId()).isEqualTo(id);
+    }
+
+    @Test
+    @DisplayName("전체 조회를 테스트합니다.")
+    void findAllTest() {
+        // Given
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        // When
+        Page<ImageDto> all = imageService.findAll(pageRequest);
+
+        // Then
+        assertThat(all.getTotalElements()).isEqualTo(id);
+    }
+
+
+//    @Test
+//    @DisplayName("수정 작업을 테스트합니다.")
+//    void updateTest() {
+//        // Given
+//
+//
+//        // When
+//
+//        // Then
+//    }
 
 
 
