1e5be0c38705198436cf23345f7938a974ea7efe
sds1vrk <sdsvrk@gmail.com>
2021-10-29 15:11:28 +0900

    DDAAN-97 oauth2.0 개발

--- Ddaangn/src/main/java/com/dev/ddaangn/user/config/auth/dto/SessionUser.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/user/config/auth/dto/SessionUser.java
@@ -1,20 +1,17 @@
-package com.dev.ddaangn.user.dto;
+package com.dev.ddaangn.user.config.auth.dto;
 
 import com.dev.ddaangn.user.User;
 import lombok.Getter;
 
 import java.io.Serializable;
 
-/**
- * 직렬화 기능을 가진 User클래스
- */
 @Getter
 public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user){
+    public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
