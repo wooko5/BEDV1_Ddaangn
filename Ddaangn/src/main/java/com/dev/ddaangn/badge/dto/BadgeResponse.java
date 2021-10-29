b2e8d368fde99e3c85e56c837a9b1009b91fcd3b
wooko5 <wooko5@naver.com>
2021-10-30 01:11:08 +0900

    FEAT: BadgeResponse 생성

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeResponse.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeResponse.java
@@ -1,21 +1,28 @@
 package com.dev.ddaangn.badge.dto;
 
 import com.dev.ddaangn.badge.Badge;
+import com.dev.ddaangn.badge.BadgeImage;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @EqualsAndHashCode
 public class BadgeResponse{
-    private Long badgeId;
-    private String name;
-    private String description;
-    private boolean achievement;
+    private final Long badgeId;
+    private final String name;
+    private final String description;
+    private final boolean achievement;
+    private final Long userId;
+    private final List<BadgeImage> badgeImages;
 
     public BadgeResponse(Badge badge){
         this.badgeId = badge.getId();
         this.name = badge.getName();
         this.description = badge.getDescription();
         this.achievement = badge.isAchievement();
+        this.userId = badge.getUser().getId();
+        this.badgeImages = badge.getBadgeImages();
     }
 }
