de6d4a0eafd240c41b4d0661fd92f391a073fbba
wooko5 <wooko5@naver.com>
2021-10-30 01:10:54 +0900

    FEAT: BadgeRequest 생성

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
@@ -1,12 +1,15 @@
 package com.dev.ddaangn.badge.dto;
 
+import com.dev.ddaangn.badge.BadgeImage;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class BadgeRequest {
-    private Long userId;
     private String name;
     private String description;
+    private List<BadgeImage> badgeImages;
 }
