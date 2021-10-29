9f61e5e0b275e320b73356a02e06ee9674279f53
sds1vrk <sdsvrk@gmail.com>
2021-10-29 10:47:21 +0900

    DDAAN-92 Controller, Service 코드 작성

--- Ddaangn/src/main/java/com/dev/ddaangn/DdaangnApplication.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/DdaangnApplication.java
@@ -1,12 +1,28 @@
 package com.dev.ddaangn;
 
+import com.dev.ddaangn.user.config.UserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @EnableJpaAuditing
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.dev.ddaangn.user"})
 public class DdaangnApplication {
+
+    private final UserArgumentResolver userArgumentResolver;
+
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//        super.UserArgumentResolver(argumentResolvers);
+        argumentResolvers.add(userArgumentResolver);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DdaangnApplication.class, args);
