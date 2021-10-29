923df7ec1800271cdfa263f2f3fff49dd9963800
wooko5 <wooko5@naver.com>
2021-10-30 01:42:36 +0900

    FEAT: 배지를 저장하기 위한 save() 생성

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/service/BadgeService.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/service/BadgeService.java
@@ -1,8 +1,13 @@
 package com.dev.ddaangn.badge.service;
 
+import com.dev.ddaangn.badge.Badge;
 import com.dev.ddaangn.badge.converter.BadgeConverter;
+import com.dev.ddaangn.badge.dto.BadgeRequest;
 import com.dev.ddaangn.badge.dto.BadgeResponse;
 import com.dev.ddaangn.badge.repository.BadgeRepository;
+import com.dev.ddaangn.common.error.ErrorMessage;
+import com.dev.ddaangn.common.error.exception.NotFoundException;
+import com.dev.ddaangn.user.User;
 import com.dev.ddaangn.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,6 +25,21 @@ public class BadgeService {
         this.badgeRepository = badgeRepository;
         this.badgeConverter = badgeConverter;
         this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public BadgeResponse save(BadgeRequest request) {
+        User user = getUser(request.getUserId());
+        Badge badge = badgeConverter.converterBadge(request, user);
+        badge.setUser(user);
+        Badge insertedBadge = badgeRepository.save(badge);
+        return new BadgeResponse(insertedBadge);
+    }
+
+    @Transactional
+    public User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_EXIST_MEMBER));
     }
 
     @Transactional
