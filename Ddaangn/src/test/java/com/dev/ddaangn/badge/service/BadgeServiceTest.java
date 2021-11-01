74cc4b1dd64e9b03a49a7b502fa3d2a7b5177a4a
wooko5 <wooko5@naver.com>
2021-11-02 02:42:46 +0900

    FEAT: BadgeServiceTest setUp(), cleanUp

--- Ddaangn/src/test/java/com/dev/ddaangn/badge/service/BadgeServiceTest.java
+++ Ddaangn/src/test/java/com/dev/ddaangn/badge/service/BadgeServiceTest.java
@@ -1,27 +1,31 @@
 package com.dev.ddaangn.badge.service;
 
-import com.dev.ddaangn.badge.Badge;
 import com.dev.ddaangn.badge.BadgeImage;
+import com.dev.ddaangn.badge.dto.BadgeImageDto;
 import com.dev.ddaangn.badge.dto.BadgeRequest;
 import com.dev.ddaangn.badge.dto.BadgeResponse;
 import com.dev.ddaangn.badge.repository.BadgeRepository;
-import com.dev.ddaangn.image.dto.ImageDto;
-import com.dev.ddaangn.image.repository.ImageRepository;
-import com.dev.ddaangn.image.service.ImageService;
+import com.dev.ddaangn.user.Role;
 import com.dev.ddaangn.user.User;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import com.dev.ddaangn.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class BadgeServiceTest {
 
     @Autowired
@@ -30,23 +34,48 @@ class BadgeServiceTest {
     @Autowired
     private BadgeService badgeService;
 
-    private final Long id = 1L;
+    @Autowired
+    private UserRepository userRepository;
+
+    private Long id = 1L;
 
     private List<BadgeImage> badgeImages = new ArrayList<>();
 
-    private User user = new User();
+    private List<BadgeImageDto> badgeImageDtos = new ArrayList<>();
+
+//    private MockMvc mvc;
+//
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
 
     @Test
     void setUp() {
+        User user = User.builder()
+                .name("재욱")
+                .email("yahoo@naver.com")
+                .address("Seoul/Asia")
+                .picture("c:wooko/img12312.png")
+                .phoneNumber("010-6693-1234")
+                .temperature(36.0)
+                .role(Role.GUEST)
+                .build();
+        userRepository.save(user);
+
         BadgeRequest request = BadgeRequest.builder() // Given
                 .name("오늘의 친절상")
                 .description("오늘의 친절상에 대한 상세 설명")
-                .badgeImages(badgeImages)
-                .userId(id)
+                .achievement(true)
+                .badgeImageDto(badgeImageDtos)
+                .userId(user.getId())
                 .build();
 
         BadgeResponse savedResponse = badgeService.save(request); // When
+        assertThat(savedResponse.getName()).isEqualTo("오늘의 친절상"); // Then
+    }
 
-        assertThat(savedResponse).isEqualTo(id); // Then
+    @AfterEach
+    void cleanUp() {
+        badgeRepository.deleteAll();
+        userRepository.deleteAll();
     }
 }
