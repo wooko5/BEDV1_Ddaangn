f1c39f428fbf6a38a3f78a900be278ff20a08006
wooko5 <wooko5@naver.com>
2021-11-02 02:45:04 +0900

    FEAT: BadgeImage에 setBadge 메소드 생성

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/BadgeImage.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/BadgeImage.java
@@ -4,6 +4,7 @@ import com.dev.ddaangn.image.Image;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.Objects;
 
 @NoArgsConstructor
 @Getter
@@ -12,4 +13,12 @@ public class BadgeImage extends Image {
     @ManyToOne(fetch = FetchType.LAZY) // BadgeImage - Badge
     @JoinColumn(name = "badge", referencedColumnName = "id")
     private Badge badge;
+
+    public void setBadge(Badge badge){
+        if(Objects.nonNull(this.badge)){
+            this.badge.getBadgeImages().remove(this);
+        }
+        this.badge = badge;
+        badge.getBadgeImages().add(this);
+    }
 }
