3de0f782c8b4f1c02de3315a75705d1a0c72d3d8
SEO DONGSUNG <51287886+sds1vrk@users.noreply.github.com>
2021-11-01 15:47:06 +0900

    Feature/ddaan 43 (#4)
    * DDAAN-92 Controller, Service 코드 작성
    
    * application-oauth.properties 추가
    
    * DDAAN-97 oauth2.0 개발
    
    * oauth 2.0 적용 및 DB 저장 (google, naver) 현재 카카오는 이슈
    
    Co-authored-by: 황일용 <1yongs_@naver.com>

--- Ddaangn/src/main/java/com/dev/ddaangn/user/User.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/user/User.java
@@ -15,46 +15,84 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Entity(name = "users")
 @DynamicUpdate
-@Builder
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    private String phoneNumber;
+    @Column(nullable = false)
+    private String email;
 
-    private String address;
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture=picture;
+        this.role=role;
+    }
+
+
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 
     private Double temperature;
 
     // 연관관계 매핑
 
     // User - Badge
-    @OneToMany(mappedBy = "user")
-    private List<Badge> badges = new ArrayList<>();
-
-    // User - AvatarImage TODO, Image 상속받고 만들기
 //    @OneToMany(mappedBy = "user")
-//    private List<AvatarImage> avatarImages = new ArrayList<>();
-
-    // User - Evaluation, 받은 평가
-    @OneToMany(mappedBy = "evaluatee")
-    private List<Evaluation> givenEvaluations = new ArrayList<>();
-
-    // User가 준 평가
-    @OneToMany(mappedBy = "evaluator")
-    private List<Evaluation> givingEvaluations = new ArrayList<>();
+//    private List<Badge> badges = new ArrayList<>();
+//
+//    // User - AvatarImage TODO, Image 상속받고 만들기
+////    @OneToMany(mappedBy = "user")
+////    private List<AvatarImage> avatarImages = new ArrayList<>();
+//
+//    // User - Evaluation, 받은 평가
+//    @OneToMany(mappedBy = "evaluatee")
+//    private List<Evaluation> givenEvaluations = new ArrayList<>();
+//
+//    // User가 준 평가
+//    @OneToMany(mappedBy = "evaluator")
+//    private List<Evaluation> givingEvaluations = new ArrayList<>();
+//
+//    // User - Post, seller
+//    @OneToMany(mappedBy = "seller")
+//    private List<Post> soldPosts = new ArrayList<>();
+//
+//    // User - Post, buyer
+//    @OneToMany(mappedBy = "buyer")
+//    private List<Post> boughtPosts = new ArrayList<>();
+//
+//    // User - Like
+//    @OneToMany(mappedBy = "user")
+//    private List<Like> likes = new ArrayList<>();
 
     // User - Post, seller
     @Embedded
@@ -64,7 +102,4 @@ public class User extends BaseEntity {
     @Embedded
     private BoughtPosts boughtPosts;
 
-    // User - Like
-    @OneToMany(mappedBy = "user")
-    private List<Like> likes = new ArrayList<>();
 }
