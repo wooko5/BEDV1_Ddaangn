43c524c2b7ab3ea8e59b8d7b3384d276096b89e2
wooko5 <wooko5@naver.com>
2021-11-02 02:44:22 +0900

    FEAT: BadgeConverter 수정

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/converter/BadgeConverter.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/converter/BadgeConverter.java
@@ -1,9 +1,13 @@
 package com.dev.ddaangn.badge.converter;
 
 import com.dev.ddaangn.badge.Badge;
+import com.dev.ddaangn.badge.BadgeImage;
+import com.dev.ddaangn.badge.dto.BadgeImageDto;
 import com.dev.ddaangn.badge.dto.BadgeRequest;
 import com.dev.ddaangn.user.User;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class BadgeConverter {
@@ -12,9 +16,9 @@ public class BadgeConverter {
         return Badge.builder()
                 .name(request.getName())
                 .description(request.getDescription())
-                .achievement(false)
+                .achievement(request.isAchievement())
+                .badgeImages(request.ListDtoToListEntity(request.getBadgeImageDto()))
                 .user(user)
-                .badgeImages(request.getBadgeImages())
                 .build();
     }
 }
