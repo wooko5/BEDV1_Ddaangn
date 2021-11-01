17fb5096b6892ea9eaf07a4e796c51fe2179dcce
dlfdyd96 <1yongs_@naver.com>
2021-11-01 18:37:08 +0900

    FEAT: Post 수정 기능 + test code

--- Ddaangn/src/main/java/com/dev/ddaangn/post/service/PostService.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/post/service/PostService.java
@@ -5,6 +5,7 @@ import com.dev.ddaangn.common.error.exception.NotFoundException;
 import com.dev.ddaangn.post.converter.PostConverter;
 import com.dev.ddaangn.post.domain.Post;
 import com.dev.ddaangn.post.dto.request.PostInsertRequest;
+import com.dev.ddaangn.post.dto.request.PostUpdateRequest;
 import com.dev.ddaangn.post.dto.response.PostDetailResponse;
 import com.dev.ddaangn.post.dto.response.PostInsertResponse;
 import com.dev.ddaangn.post.repository.PostRepository;
@@ -45,10 +46,18 @@ public class PostService {
     }
 
     @Transactional
-    public PostDetailResponse findById(Long userId) {
-        return postRepository.findById(userId)
+    public PostDetailResponse findById(Long postId) {
+        return postRepository.findById(postId)
                 .map(PostDetailResponse::new)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_EXIST_MEMBER));
+    }
+
+    @Transactional
+    public PostDetailResponse update(Long postId, PostUpdateRequest request) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_EXIST_MEMBER));
+        post.update(request);
+        return new PostDetailResponse(post);
     }
 
     @Transactional
