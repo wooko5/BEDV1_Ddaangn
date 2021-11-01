ff6d433676224d8030e715aa0b564fd3a2bf2659
wooko5 <wooko5@naver.com>
2021-11-02 02:44:01 +0900

    FEAT: List<BadgeImageDto>를 List<BadgeImage>로 만들 메소드 생성

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
@@ -1,10 +1,12 @@
 package com.dev.ddaangn.badge.dto;
 
+import com.dev.ddaangn.badge.Badge;
 import com.dev.ddaangn.badge.BadgeImage;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -12,6 +14,12 @@ public class BadgeRequest {
     private String name;
     private String description;
     private boolean achievement;
-    private List<BadgeImage> badgeImages;
+    private List<BadgeImageDto> badgeImageDto;
     private Long userId;
+
+    public List<BadgeImage> ListDtoToListEntity(List<BadgeImageDto> badgeImageDtos){
+        return badgeImageDtos.stream()
+                .map(BadgeImageDto::toEntity)
+                .collect(Collectors.toList());
+    }
 }
