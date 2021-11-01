932350fb417d85f4e44614ce04464c74ac3523a8
wooko5 <wooko5@naver.com>
2021-11-02 01:08:53 +0900

    FEAT: common.error.exception.NotFoundException을 쓰기 위한 import 수정

--- Ddaangn/src/main/java/com/dev/ddaangn/image/controller/ImageController.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/image/controller/ImageController.java
@@ -3,9 +3,10 @@ package com.dev.ddaangn.image.controller;
 import com.dev.ddaangn.common.api.ApiResponse;
 import com.dev.ddaangn.image.dto.ImageDto;
 import com.dev.ddaangn.image.service.ImageService;
-import javassist.NotFoundException;
+import com.dev.ddaangn.common.error.exception.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +21,7 @@ public class ImageController {
 
 
     @GetMapping("/") // 이미지 다건 조회
+    @ResponseStatus(HttpStatus.OK)
     public ApiResponse<Page<ImageDto>> getAll(Pageable pageable) {
         Page<ImageDto> all = imageService.findAll(pageable);
         return ApiResponse.ok(all);
@@ -27,13 +29,15 @@ public class ImageController {
 
 
     @GetMapping("/{id}") // 이미지 단건 조회
-    public ApiResponse<ImageDto> getOne(@PathVariable Long id) throws NotFoundException {
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<ImageDto> getOne(@PathVariable Long id){
         ImageDto imageDto = imageService.findOneById(id);
         return ApiResponse.ok(imageDto);
     }
 
     // 이미지 작성
     @PostMapping("/") // 게시글 생성
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<Long> save(@RequestBody ImageDto imageDto) {
         Long id = imageService.save(imageDto);
         return ApiResponse.ok(id);
@@ -41,8 +45,8 @@ public class ImageController {
 
     // 이미지 수정
     @PutMapping("/{id}")
-    public ApiResponse<ImageDto> update(@RequestBody ImageDto imageDto) throws NotFoundException {
-        ImageDto updatedImage = imageService.update(imageDto.getId(), imageDto.getUrl(), imageDto.getType());
+    public ApiResponse<ImageDto> update(@RequestBody ImageDto imageDto, @PathVariable Long id) throws NotFoundException {
+        ImageDto updatedImage = imageService.update(id, imageDto.getUrl(), imageDto.getType());
         return ApiResponse.ok(updatedImage);
     }
 
