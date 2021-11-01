78c311e9849f013f6b8b01d2350b710ee51f550b
wooko5 <wooko5@naver.com>
2021-11-02 01:44:27 +0900

    FEAT: BadgeRequest에 boolean achievement 추가

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeRequest.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class BadgeRequest {
     private String name;
     private String description;
+    private boolean achievement;
     private List<BadgeImage> badgeImages;
     private Long userId;
 }
