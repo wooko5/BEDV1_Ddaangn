e16416e597083884b536752ba378fc31a262cb14
wooko5 <wooko5@naver.com>
2021-11-02 18:41:21 +0900

    FEAT: @EqualsAndHashCode 풀어쓰기

--- Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeImageDto.java
+++ Ddaangn/src/main/java/com/dev/ddaangn/badge/dto/BadgeImageDto.java
@@ -1,16 +1,17 @@
 package com.dev.ddaangn.badge.dto;
 
-import com.dev.ddaangn.badge.Badge;
 import com.dev.ddaangn.badge.BadgeImage;
+import com.dev.ddaangn.image.Image;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Builder
-@EqualsAndHashCode
+@AllArgsConstructor
 public class BadgeImageDto {
     private LocalDateTime createdAt;
     private LocalDateTime updateAt;
@@ -20,10 +21,28 @@ public class BadgeImageDto {
     private String type;
 
     public BadgeImage toEntity(){
-        return (BadgeImage) BadgeImage.builder()
+        return (BadgeImage)BadgeImage.builder()
                 .id(id)
                 .url(url)
                 .type(type)
                 .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BadgeImageDto)) return false;
+        BadgeImageDto that = (BadgeImageDto) o;
+        return Objects.equals(createdAt, that.createdAt)
+                && Objects.equals(updateAt, that.updateAt)
+                && Objects.equals(deletedAt, that.deletedAt)
+                && Objects.equals(id, that.id)
+                && Objects.equals(url, that.url)
+                && Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(createdAt, updateAt, deletedAt, id, url, type);
     }
 }
